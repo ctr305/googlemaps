59f52125995989a36ffc36fc1616ecbabab600ac
Leonardo Criollo <criollo_banneret@tutanota.com>
2023-11-25 14:19:48 -0600

    Commit inicial.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using googlemaps.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace googlemaps.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        var Transportes = new Dictionary<string, string>
+        {
+            { "DRIVING", "Automóvil" },
+            { "WALKING", "Caminando" },
+            { "BICYCLING", "Bicicleta" },
+            { "TRANSIT", "Transporte público" }
+        };
+        var selectList = new SelectList(Transportes, "Key", "Value");
+
+        var model = new UbicacionViewModel { Transportes = selectList, MostrarRuta = true };
+        return View(model);
+    }
+}
